4f38e25da1b0e14d57d27a78a0a4961b741cedf4
Greg King <gregdking@gmail.com>
2014-11-21 15:54:06 -0500

    remove unneeded libraries

--- modules/main.js
+++ modules/main.js
@@ -1,11 +1,9 @@
-﻿/// <reference path="../scripts/require.js" />
-
-requirejs.config({
+﻿requirejs.config({
     baseUrl: 'modules', // Change this setting to point to your own module folder to test your own code.
     paths: {
         jquery: '../lib/jquery-2.0.3',
         underscore: '../lib/underscore',
-        'knockout': '../lib/knockout-2.3.0'
+        knockout: '../lib/knockout-2.3.0'
     },
     shim: {
         'underscore': {
@@ -13,19 +11,12 @@ requirejs.config({
         },
         'jquery': {
             exports: '$'
-        },
-        'jasmine-html': {
-            deps: ['jasmine']
-        },
-        'd3': {
-            exports: 'd3'
         }
     }
-   // ,urlArgs: '_ts=' + (new Date()).getTime()
 });
 
 var done = false;
-requirejs(['knockout', './board'], function (ko, Board) {
+requirejs(['knockout', 'board'], function (ko, Board) {
     if (!done) {
         var gameBoard = new Board({ height: 100, width: 100, ratio: 0.2 });
         ko.applyBindings(gameBoard);
